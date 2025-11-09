30700d2a40f1c5aaaaf8e547e7091473d05b2944
Riya Kaushik Dharsandiya <riyadharsandiya@Riyas-MacBook-Air-2.local>
2025-11-09 22:18:50 +0530

    payment

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,7 +1,7 @@
 const Footer = () => {
     return (
       <footer className="bg-[#3e2c1c] text-white text-center py-4">
-        <p>Created by Riya © 2025 All rights reserved</p>
+        <p>Created by Riya Dharsandiya © 2025 All rights reserved</p>
       </footer>
     );
   };
