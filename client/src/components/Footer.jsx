3eeed251f62612ac17f2795b9bda3b9e899bbea4
Riya Kaushik Dharsandiya <riyadharsandiya@Riyas-MacBook-Air-2.local>
2025-11-10 10:42:24 +0530

    year

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,7 +1,7 @@
 const Footer = () => {
     return (
       <footer className="bg-[#3e2c1c] text-white text-center py-4">
-        <p>Brew Hop © 2025 All rights reserved</p>
+        <p>Brew Hop © {new Date().getFullYear()} All rights reserved</p>
       </footer>
     );
   };
